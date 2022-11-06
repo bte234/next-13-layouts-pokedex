f5eb4d23a40f54496f127a2bbedbbdf2f4ce92a4
Huy Anh Nguyen <26082352+bte234@users.noreply.github.com>
2022-11-06 13:42:49 -0800

    feat: rsc for form

--- app/page.tsx
+++ app/page.tsx
@@ -1,38 +1,13 @@
-"use client";
-
-import { useRouter } from "next/navigation";
-import { FormEvent, useState } from "react";
-
-const formatInput = (str: string) => {
-  return str
-    .replace(/♂/gi, "-m")
-    .replace(/♀/gi, "-f")
-    .replace(/é/gi, "e")
-    .replaceAll(/[^\w\s-]/gi, "")
-    .split(" ")
-    .join("-")
-    .toLowerCase();
-};
-
 export default function Home() {
-  const [text, setText] = useState("");
-  const router = useRouter();
-
-  const onSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    router.push(`/${formatInput(text)}`);
-  };
-
   return (
     <>
       <h1 className="text-5xl font-bold text-slate-70 mb-8">Find Pokemon</h1>
-      <form onSubmit={onSubmit} className="space-y-8">
+      <form className="space-y-8" action="/api/pokemon">
         <label className="flex flex-col gap-2">
           <span>Enter pokemon name</span>
           <input
             type="text"
-            value={text}
-            onChange={(e) => setText(e.target.value)}
+            name="pokemon"
             className="py-2 px-6 border-slate-200 rounded border"
           />
         </label>
---
+++ pages/api/pokemon.ts
@@ -0,0 +1,17 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const formatInput = (str: string) => {
+  return str
+    .replace(/♂/gi, "-m")
+    .replace(/♀/gi, "-f")
+    .replace(/é/gi, "e")
+    .replaceAll(/[^\w\s-]/gi, "")
+    .split(" ")
+    .join("-")
+    .toLowerCase();
+};
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  const query = req.query;
+  res.redirect(`/${formatInput(query.pokemon)}`);
+}
